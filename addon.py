57ed81d45f37d4a531e97ec94e82be5c814e3499
sophof <sjoerd.ophof@gmail.com>
2017-01-17 20:14:55 +0100

    Added latest and acht uur journaal items

--- addon.py
+++ addon.py
@@ -19,13 +19,12 @@ uitzendingen_url = 'http://nos.nl/uitzending/nos-journaal.html'
 nos_prefix = 'http://nos.nl'
 regex_journaals = SoupStrainer(href=re.compile('/uitzending/[0-9]+-nos-journaal.html'))
 
-def play_url(url):
+def build_url(query):
     """
-    build plugin url with action=play
-    :param url: str
+    build plugin url with the provided query
+    :param query: dict
     :return: None
     """
-    query = {'action': 'play', 'url':url}
     return (pluginurl + '?' + urlencode(query))
 
 def list():
@@ -33,30 +32,53 @@ def list():
     create root folder listing all possible streams
     :return: None
     """
+    li = xbmcgui.ListItem('Laatste Journaal', iconImage='DefaultVideo.png')
+    li.setProperty('IsPlayable', 'true')
+    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_url({'action':'latest'}), listitem=li)
+    li = xbmcgui.ListItem('Acht uur Journaal', iconImage='DefaultVideo.png')
+    li.setProperty('IsPlayable', 'true')
+    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_url({'action':'acht'}), listitem=li)
+
     journaals = get_journaals()
     for journaal in journaals:
         li = xbmcgui.ListItem(journaal['time'], iconImage='DefaultVideo.png')
         li.setProperty('IsPlayable', 'true')
-        xbmcplugin.addDirectoryItem(handle=pluginhandle, url=play_url(journaal['url']), listitem=li)
+        query = {'action': 'play', 'url':journaal['url']}
+        xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_url(query), listitem=li)
     xbmcplugin.endOfDirectory(pluginhandle)
+
+def parse_site():
+    r = requests.get(uitzendingen_url)
+    html = BeautifulSoup(r.text,'html.parser', parse_only=regex_journaals)
+    return html
 
 def get_journaals():
     """
     get a list of journaals from the url
     :return: list of dicts. dict has 'url' and 'time' keys
     """
-    r = requests.get(uitzendingen_url)
-    html = BeautifulSoup(r.text,'html.parser', parse_only=regex_journaals)
-    
+    html = parse_site()
     journaals = []
     for link in html:
-        journaal = {}
-        journaal['url'] = nos_prefix + link.get('href')
-        journaal['time'] = link.time.text.strip()
+        journaal = {} 
+        journaal['url'] = nos_prefix + link.get('href') 
+        journaal['time'] = link.time.text.strip() 
         journaals.append(journaal)
-        
+
     return journaals
-    
+
+def play_latest():
+    html = parse_site()
+    url = nos_prefix + html.a.get('href')
+    play_stream(url)
+
+def play_acht():
+    html = parse_site()
+    for link in html:
+        if link.time.text.strip() == '20:00':
+            play_stream(nos_prefix + link.get('href'))
+            break
+
 def play_stream(stream_url):
     """
     Resolve and play a stream at the provided url.
@@ -72,7 +94,7 @@ def play_stream(stream_url):
     url = urls['best'].url
     # Pass the item to the Kodi player.
     xbmcplugin.setResolvedUrl(pluginhandle, True, listitem=xbmcgui.ListItem(path=url))
-    
+
 def router(paramstring):
     """
     Router function that calls other functions
@@ -88,6 +110,12 @@ def router(paramstring):
         if params['action'] == 'play':
             # Play a video from a provided URL.
             play_stream(params['url'])
+        elif params['action'] == 'latest':
+            play_latest()
+        elif params['action'] == 'acht':
+            play_acht()
+        else:
+            list()
     else:
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of streams
