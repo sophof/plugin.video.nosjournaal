2c317e215a34dd1f9c5cb672ad2414ed62ba00e2
sjoerd ophof <sjoerd ophof>
2017-01-17 17:39:57 +0100

    - get a list of journaal urls - create  folder in kodi - resolve and play items

--- addon.py
+++ addon.py
@@ -2,6 +2,9 @@ import sys
 import os.path
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 import streamlink
+from bs4 import BeautifulSoup
+import requests
+import re
 from urlparse import parse_qsl
 from urllib import urlencode
 
@@ -11,6 +14,9 @@ pluginurl = sys.argv[0]
 pluginhandle = int(sys.argv[1])
 # Let kodi show the streams as a list of files
 xbmcplugin.setContent(pluginhandle, 'tvshows')
+#constants
+uitzendingen_url = 'http://nos.nl/uitzending/nos-journaal.html'
+regex_journaals = SoupStrainer(href=re.compile('/uitzending/[0-9]+-nos-journaal.html'))
 
 def play_url(url):
     """
@@ -26,13 +32,45 @@ def list():
     create root folder listing all possible streams
     :return: None
     """
+    journaals = get_journaals()
+    for journaal in journaals:
+        li = xbmcgui.ListItem(journaal['time'], iconImage='DefaultVideo.png')
+        li.setProperty('IsPlayable', 'true')
+        xbmcplugin.addDirectoryItem(handle=pluginhandle, url=play_url(journaal['url']), listitem=li)
+    xbmcplugin.endOfDirectory(pluginhandle)
 
+def get_journaals():
+    """
+    get a list of journaals from the url
+    :return: list of dicts. dict has 'url' and 'time' keys
+    """
+    r = requests.get(uitzendingen_url)
+    html = BeautifulSoup(r.text,'html.parser', parse_only=regex_journaals)
+    
+    journaals = []
+    for link in html:
+        journaal = {}
+        journaal['url'] = link.get('href')
+        journaal['time'] = link.time.text.strip()
+        journaals.append(journaal)
+        
+    return journaals
+    
 def play_stream(stream_url):
     """
     Resolve and play a stream at the provided url.
     :param stream_url: str
     :return: None
     """
+    try:
+        urls = streamlink.streams(stream_url)
+    except streamlink.exceptions.NoPluginError:
+        xbmcgui.Dialog().notification('Unable to play stream', 'no plugin for stream at {}'.format(stream_url), xbmcgui.NOTIFICATION_ERROR, 5000)
+        return
+
+    url = urls['best'].url
+    # Pass the item to the Kodi player.
+    xbmcplugin.setResolvedUrl(pluginhandle, True, listitem=xbmcgui.ListItem(path=url))
     
 def router(paramstring):
     """
