81f06d93f93beb086d49ce8a2ac3950f13232193
sjoerd ophof <sjoerd ophof>
2017-01-23 13:09:42 +0100

    - rewrote get_journaals using yield instead of returning a list of dicts. Adjusted other functions to use this better approach. Less code and faster than before - Added setinfo for each listitem. Prevents watched flag and streaminput errors in kodi log

--- addon.py
+++ addon.py
@@ -13,13 +13,13 @@ pluginurl = sys.argv[0]
 # Get the plugin handle as an integer number.
 pluginhandle = int(sys.argv[1])
 # Let kodi show the streams as a list of files
-xbmcplugin.setContent(pluginhandle, 'tvshows')
+xbmcplugin.setContent(pluginhandle, 'video')
 #constants
 uitzendingen_url = 'http://nos.nl/uitzending/nos-journaal.html'
 nos_prefix = 'http://nos.nl'
 regex_journaals = SoupStrainer(href=re.compile('/uitzending/[0-9]+-nos-journaal.html'))
 
-def build_url(query):
+def build_plugin_url(query):
     """
     build plugin url with the provided query
     :param query: dict
@@ -34,49 +34,43 @@ def list():
     """
     li = xbmcgui.ListItem('Laatste Journaal', iconImage='DefaultVideo.png')
     li.setProperty('IsPlayable', 'true')
-    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_url({'action':'latest'}), listitem=li)
+    li.setInfo('video', infoLabels={'Title':'Laatste Journaal','mediatype':'video'})
+    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_plugin_url({'action':'latest'}), listitem=li)
     li = xbmcgui.ListItem('Acht uur Journaal', iconImage='DefaultVideo.png')
     li.setProperty('IsPlayable', 'true')
-    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_url({'action':'acht'}), listitem=li)
+    li.setInfo('video', infoLabels={'Title':'Laatste Acht uur Journaal','mediatype':'video'})
+    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_plugin_url({'action':'acht'}), listitem=li)
 
-    journaals = get_journaals()
-    for journaal in journaals:
-        li = xbmcgui.ListItem(journaal['time'], iconImage='DefaultVideo.png')
+    for journaal in get_journaals():
+        title = journaal['time'] + ' Journaal'
+        li = xbmcgui.ListItem(title, iconImage='DefaultVideo.png')
         li.setProperty('IsPlayable', 'true')
+        li.setInfo('video', infoLabels={'Title':title,'mediatype':'video'})
         query = {'action': 'play', 'url':journaal['url']}
-        xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_url(query), listitem=li)
+        xbmcplugin.addDirectoryItem(handle=pluginhandle, url=build_plugin_url(query), listitem=li)
     xbmcplugin.endOfDirectory(pluginhandle)
-
-def parse_site():
-    r = requests.get(uitzendingen_url)
-    html = BeautifulSoup(r.text,'html.parser', parse_only=regex_journaals)
-    return html
 
 def get_journaals():
     """
-    get a list of journaals from the url
-    :return: list of dicts. dict has 'url' and 'time' keys
+    get an iterator over journaals from the url
+    :return: an iterator over journaal dicts containing 'url' and 'time'
     """
-    html = parse_site()
-    journaals = []
+    r = requests.get(uitzendingen_url)
+    html = BeautifulSoup(r.text,'html.parser', parse_only=regex_journaals)
     for link in html:
-        journaal = {} 
         journaal['url'] = nos_prefix + link.get('href') 
-        journaal['time'] = link.time.text.strip() 
-        journaals.append(journaal)
-
-    return journaals
+        journaal['time'] = link.time.text.strip()
+        yield journaal
 
 def play_latest():
-    html = parse_site()
-    url = nos_prefix + html.a.get('href')
-    play_stream(url)
+    for journaal in get_journaals():
+        play_stream(journaal['url'])
+        break #only do it for the first item in the list
 
 def play_acht():
-    html = parse_site()
-    for link in html:
-        if link.time.text.strip() == '20:00':
-            play_stream(nos_prefix + link.get('href'))
+    for journaal in get_journaals():
+        if journaal['time'] == '20:00':
+            play_stream(journaal['url'])
             break
 
 def play_stream(stream_url):
